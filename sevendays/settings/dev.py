7f3a6d08592a9e90df103513bc2566e48ec5b8bb
benjaminpritchard230 <104660463+benjaminpritchard230@users.noreply.github.com>
2022-10-30 00:10:15 +0100

    Learning wagtail

---
+++ sevendays/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-2pc!sa_rzh!dr@k77=cu^1by5o%8v__0@o+wqlk!9z66o$b5=)"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
